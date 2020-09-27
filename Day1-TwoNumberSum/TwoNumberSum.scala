b6b364c5deed82dd66be132c99922a97476fdf1c
phuccoi996@gmail.com <phuccoi996@gmail.com>
2020-09-27 16:43:09 +0900

    auto updates

--- Day1-TwoNumberSum/TwoNumberSum.scala
+++ Day1-TwoNumberSum/TwoNumberSum.scala
@@ -1,29 +1,30 @@
-import java.util._;
+import java.util.Arrays;
 
 object SumTwoNumbers {
-  def main(args: Array[String]): Unit = {    
+  def main(args: Array[String]): Unit = {
     val input_array =  Array( -21, 301, 12, 4, 65, 56, 210, 356, 9, -47)
     println(twoNumberSum(input_array,163).toList)
 
   }
 
-
   def twoNumberSum(array: Array[Int], targetSum: Int ): Array[Int] = {
-      var left = 0;
-      var right = array.length -1;
+    Arrays.sort(array);
+    var left = 0;
+    var right = array.length -1;
+    while (left < right){
       var currentSum = array(left) + array(right);
-      while (left < right){
-        if (currentSum == targetSum){
-          return Array(array(left), array(right));
-        }
-        else if (currentSum < targetSum){
-          left+=1;
-        }
-        else if (currentSum > targetSum){
-          right-=1;
-        }
+      if (currentSum == targetSum){
+        return Array(array(left), array(right));
       }
-      return Array()
+      else if (currentSum < targetSum){
+        left+=1;
+      }
+      else if (currentSum > targetSum){
+        right-=1;
+      }
+    }
+    return Array()
   }
 }
+
 
