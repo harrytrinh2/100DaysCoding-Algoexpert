61d361a223c89f11feabc82b718a1c1b6d43eec9
phuccoi996@gmail.com <phuccoi996@gmail.com>
2020-09-27 16:20:26 +0900

    auto updates

---
+++ Day1-TwoNumberSum/TwoNumberSum.scala
@@ -0,0 +1,13 @@
+import java.util._;
+
+object SumTwoNumbers {
+  def main(args: Array[String]): Unit = {
+    val array = Array(1,5,2,11,-3,1,2,9);
+    println(sumTwoNumbers(array,16).toList)
+  }
+
+  def sumTwoNumbers(array: Array[Int],targetNumber : Int) : Array[Int] = {
+    return array
+  }
+}
+
