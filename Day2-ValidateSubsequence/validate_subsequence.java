cd6e05963953079124d7fc6879980ed679dd0192
phuccoi996@gmail.com <phuccoi996@gmail.com>
2020-09-29 15:30:39 +0900

    auto updates

---
+++ Day2-ValidateSubsequence/validate_subsequence.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Main {
+
+  public static void main(String[] args) {
+    int[] input_array =  new int[]{ 5,1,22,25,6,-1,8,10};
+    int[] input_sequence = new int[]{1,6,-1,10};
+    System.out.println(validateSequence(input_array, input_sequence));
+
+  }
+
+  public static boolean validateSequence(int[] array, int[] sequence) {	
+    // Write your code here.
+    int idxArr = 0;
+    int idxSeq = 0;
+    while( idxArr < array.length && idxSeq < sequence.length ){
+      if (array[idxArr] == sequence[idxSeq]){
+        idxSeq +=1;
+      }
+      idxArr+=1;
+    }
+    return idxSeq == sequence.length;
+  }
+}
