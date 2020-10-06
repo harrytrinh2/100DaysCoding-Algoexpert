dfe292e02c83c9b6ca7c259359a095885dd79cb1
TrinhDinhPhuc <phuccoi996@gmail.com>
2020-10-07 02:06:55 +0900

    day5

--- trial.py
+++ trial.py
@@ -1,20 +1,10 @@
-# O(n^2) time | O(1) space
-def moveElementToEnd(array, toMove):
-    # Write your code here.
-	final_Array = []
-	count=0
-	for element in array:
-		if element == toMove:
-			count+=1
-		else:
-			final_Array.append(element)
-	for i in range(0,count):
-		final_Array.append(toMove)
-	return final_Array
 # O(n) time | O(1) space			
 # --------------------------------------------------------
 def moveElementToEnd(array, toMove):
-    left, right = 0, len(array) - 1
+	left  = 0
+	right = len(array) - 1 # minus 1 because array starts from 0
+	print("original_array",array)
+	print(right,array[right])
 	while left < right:
 		if array[right] == toMove:
 			right -= 1
@@ -24,3 +14,4 @@ def moveElementToEnd(array, toMove):
 			array[left], array[right] = array[right], array[left]
 	return array
 
+print(moveElementToEnd([2, 1, 2, 2, 2, 3, 4, 2],2))
