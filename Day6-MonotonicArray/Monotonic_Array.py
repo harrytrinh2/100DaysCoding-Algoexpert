c21b3f38f1e43785a38bda9d73b683f758e6eeef
phuccoi996@gmail.com <phuccoi996@gmail.com>
2020-10-07 14:39:19 +0900

    auto updates

--- Day6-MonotonicArray/Monotonic_Array.py
+++ Day6-MonotonicArray/Monotonic_Array.py
@@ -1,14 +1,14 @@
 # 0(n) time | O(1) space
 def isMonotonic(array):
     # Write your code here
-    if len(array) <= 2:
-        return True
-    for i in range(1, len(array) - 1):
-        prevN = array[i - 1]
-        currN = array[i]
-        nextN = array[i + 1]
-        if prevN <= currN <= nextN or prevN >= currN >= nextN:
-            continue
-        else:
-            return False
-    return True
+	if len(array) ==2:
+		return True
+	for i in range(1, len(array)-1):
+		prev_element = array[i-1]
+		curr_element = array[i]
+		next_element = array[i+1]
+		if prev_element <= curr_element <= next_element or prev_element >= curr_element >= next_element:
+			continue
+		else:
+			return False
+	return True
