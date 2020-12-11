8250f6da8b4b42170ec799dc2a5b3b74c0437f58
TrinhDinhPhuc <phuccoi996@gmail.com>
2020-12-12 01:53:27 +0900

    auto-updates

--- Day2-ValidateSubsequence/validate_subsequence.py
+++ Day2-ValidateSubsequence/validate_subsequence.py
@@ -1,7 +1,18 @@
 # Time: O(n) | Space: O(1)
 def isValidSubsequence(array, sequence):
-    seqIdx = 0
+    # Write your code here.
+	idx=0
+	seq_length = len(sequence)
+	for element in array:
+		if element == sequence[idx]:
+			idx+=1
+		if idx == seq_length:
+			return True
+	return False
 
+# Time: O(n) | Space: O(1)
+def isValidSubsequence(array, sequence):
+    seqIdx = 0
     for value in array:
         if seqIdx == len(sequence):
             return True
@@ -19,12 +30,10 @@ def isValidSubsequence(array, sequence):
             seqIdx += 1
         arrIdx += 1
     return seqIdx == len(sequence)		
-				
 
-array = [5,1,22,25,6,-1,8,10]
-sequence = [1,6,-1,10]
-
-# array = [1,1,1,1,1]
-# sequence = [1,1,1]
-
+#{
+#  "array": [5, 1, 22, 25, 6, -1, 8, 10],
+#  "sequence": [5, 1, 22, 22, 25, 6, -1, 8, 10]
+#}
+# result: Flase
 print(isValidSubsequence(array,sequence))
