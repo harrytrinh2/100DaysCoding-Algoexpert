c717b4cafac9b2f281f3b88cc296b88007b9f875
TrinhDinhPhuc <phuccoi996@gmail.com>
2021-01-30 01:03:41 +0900

    auto-updates

--- trial.py
+++ trial.py
@@ -1,14 +1,26 @@
+def shortenPath(path):
+	pathListTokens = path.split("/")
+	shortenPathStack = []
+	shortenPathStack.append("/",) if pathListTokens[0] == "" else None 
+	print(pathListTokens)
+	for token in pathListTokens:
+		if token == "":
+			continue
+		if token == ".." and len(shortenPathStack) > 1:
+			# print("before: ", shortenPathStack)
+			shortenPathStack.pop()
+			shortenPathStack.pop()
+			# print("after: ", shortenPathStack)
+		elif token == "." and len(shortenPathStack) > 1:
+			continue
+		else:
+			shortenPathStack.append(token)
+			shortenPathStack.append("/")
+		print(shortenPathStack)
+	if len(shortenPathStack) > 2:
+		shortenPathStack.pop()
+	return "".join(shortenPathStack)
 
-def firstDuplicateValue(array):
-	# Write your code here.
-	for i in range(0,len(array),1):
-		if array[i] < 0:
-			return abs(array[i])
-		idx = abs(array[i]) -1
-		array[idx] = - array[i] 
-	print(array)
-	return -1
 
-
-print(firstDuplicateValue([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10]))
-print(firstDuplicateValue([2, 1, 1]))
+print(shortenPath("/foo/../test/../test/../foo//bar/./baz"))
+## "/foo/bar/baz")
